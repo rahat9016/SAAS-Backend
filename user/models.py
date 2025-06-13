67701b0e0a6f560efdb16a576a21d1675f4577ab
rahat9016 <minhajurrohoman9016@gmail.com>
2025-06-14 02:20:07 +0600

    Created User model

---
+++ user/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin 
+import uuid
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password, **extra_fields):
+        if not phone:
+            raise ValueError('Phone number is required.')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(phone, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CUSTOMER = "Customer"
+    ROLE_ADMIN = "Admin"
+    ROLE_VENDOR = "Vendor"
+    ROLE_MODERATOR = "Moderator"
+
+    ROLE_CHOICESS = (
+        ("customer", ROLE_CUSTOMER),
+        ("admin", ROLE_ADMIN),
+        ("vendor", ROLE_VENDOR),
+        ("moderator", ROLE_MODERATOR),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone = models.CharField(max_length=15, unique=True)
+    profile_picture = models.ImageField(upload_to="profiles/", null=True, blank=True)
+    role = models.CharField(max_length=25, choices=ROLE_CHOICESS, default=ROLE_CUSTOMER)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    def username(self):
+        return f"{self.first_name}_${self.last_name}_{self.id.hex[:8]}"
+    
+    def __str__(self):
+        return self.email or self.phone
+    
